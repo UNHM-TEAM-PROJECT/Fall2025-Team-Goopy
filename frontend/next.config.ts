4b13240a9b94bdfc6162be3c9cb82f55a38c48af
rk1405 <rk1405@usnh.edu>
2025-09-22 01:14:36 -0400

    add Dockerfile, frontend, and scrape

---
+++ frontend/next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  output: 'export',
+  assetPrefix: '/t3/',
+  basePath: '/t3'
+};
+
+export default nextConfig;
